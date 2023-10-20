04881d6bf14176fe31ce38a159df1350462f6fd3
Jasurbek <jasmirzaev@gmail.com>
2023-10-20 06:38:51 +0300

    component TheNavigation

---
+++ components/TheNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="nav">
+        <h3>Nuxt App</h3>
+    </nav>
+</template>
+
+<style lang="css" scoped>
+.nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 40px;
+    padding: 0px 20px;
+    display: flex;
+    align-items: center;
+    background: darkblue;
+    color: #fff;
+
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+</style>
