500f2ac35973895ca0faf465c8524f4ca2c647bd
Jasurbek <jasmirzaev@gmail.com>
2023-10-23 07:04:01 +0300

    page links

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,10 +1,24 @@
 <template>
     <nav class="nav">
         <h3>Nuxt App</h3>
+        <div class="nuxt-links">
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/github">Github</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>    
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
 <style lang="css" scoped>
+
 .nav {
     position: absolute;
     top: 0;
@@ -14,9 +28,28 @@
     padding: 0px 20px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     background: darkblue;
     color: #fff;
 
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+.nuxt-links ul {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+
+.nuxt-links ul li {
+    margin-right: 0.5rem;
+}
+.nuxt-links ul li a {
+    color: #fff;
+    text-decoration: none;
+}
+
+.nuxt-links ul li a:hover {
+    text-decoration: underline;
 }
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,9 @@
     <h1>
         About Page
     </h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit corrupti tempora ipsum neque dignissimos rem? Dolorem maxime ipsum, aspernatur quia voluptatum quod, optio esse quasi excepturi ad adipisci harum quidem, reiciendis repellendus ipsam! Sint quia sunt natus vitae amet quas sapiente delectus accusantium iure, eligendi ipsa consequuntur, distinctio voluptatem nam?
+    </p>
 </template>
 
 
